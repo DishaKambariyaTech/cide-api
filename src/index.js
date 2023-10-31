bce8a700723309b6a96e911588c2bb084b315d0b
DishaKambariyaTech <144014669+DishaKambariyaTech@users.noreply.github.com>
2023-10-31 10:57:21 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const dotenv = require("dotenv").config();
 const app = require("./app");
 const port = process.env.PORT;
 
-app.listen(port, () => console.log("Server listening on port " + `${port}`));
+app.listen(port, () => console.log("Server listening on port::s " + `${port}`));
