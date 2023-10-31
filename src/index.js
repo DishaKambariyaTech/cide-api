66b8af0c8e1902b46ae2443f8ea3a251130ff852
DishaKambariyaTech <144014669+DishaKambariyaTech@users.noreply.github.com>
2023-10-31 11:10:08 +0530

    added user

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ const dotenv = require("dotenv").config();
 
 const app = require("./app");
 const port = process.env.PORT;
+const user = process.env.TEST_USER;
 
-app.listen(port, () => console.log("Server listening on port::" + `${port}`));
+app.listen(port, () => {
+  console.log("Server listening on port::" + `${port}`);
+  console.log("Running user::" + `${user}`);
+});
